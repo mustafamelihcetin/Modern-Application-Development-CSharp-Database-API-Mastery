068705109f29017177c350fd9dad74263628f52a
mustafamelihcetin <mustafamelihcetin@outlook.com>
2024-10-31 00:50:39 +0300

    Creating movie and context class, code first migration, adding category table with code first.

---
+++ Project4_EntityFrameworkCodeFirstMovie/DAL/Entities/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project4_EntityFrameworkCodeFirstMovie.DAL.Entities
+{
+    public class Movie
+    {
+        public int MovieID { get; set; }
+        public string MovieTitle { get; set; }
+        public string Category { get; set; }
+        public int Duration { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
