b5f2604c8f14c3b25c83f67824e0a80b514af1ac
Melih <mustafamelihcetin@outlook.com>
2024-11-05 15:43:31 +0300

    .

--- Project5_DapperNorthwind/Program.cs
+++ Project5_DapperNorthwind/Program.cs
@@ -16,6 +16,7 @@ namespace Project5_DapperNorthwind
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
             Application.Run(new Form1());
         }
     }
