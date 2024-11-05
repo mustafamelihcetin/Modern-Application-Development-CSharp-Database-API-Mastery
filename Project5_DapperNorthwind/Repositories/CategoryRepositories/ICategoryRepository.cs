d4eb36d8d1444509cbc97c8c22bb77c890b40aab
Melih <mustafamelihcetin@outlook.com>
2024-11-06 00:04:49 +0300

    Dto classes are created.

---
+++ Project5_DapperNorthwind/Repositories/CategoryRepositories/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project5_DapperNorthwind.Repositories.CategoryRepositories
+{
+    public interface ICategoryRepository
+    {
+
+    }
+}
