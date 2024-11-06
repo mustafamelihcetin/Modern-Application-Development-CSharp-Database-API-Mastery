5ec938daf41c237e7163bc59b51de53bda516c2b
Melih <mustafamelihcetin@outlook.com>
2024-11-06 03:35:48 +0300

    HttpGet, HttpPost methods, CRUD process in API

---
+++ Project6_ApiWeather/Controllers/WeathersController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Project6_ApiWeather.Context;
+using Project6_ApiWeather.Entities;
+
+namespace Project6_ApiWeather.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeathersController : ControllerBase
+    {
+        WeatherContext context = new WeatherContext();
+
+        [HttpGet]
+        public IActionResult WeatherCityList()
+        {
+            var values = context.Cities.ToList();
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult CreateWeatherCitiy(City city)
+        {
+            context.Cities.Add(city);
+            context.SaveChanges();
+            return Ok("Şehir eklendi.");
+        }
+        [HttpDelete]
+        public IActionResult DeleteWeatherCity(int id)
+        {
+            var value = context.Cities.Find(id);
+            context.Cities.Remove(value);
+            context.SaveChanges();
+            return Ok("Şehir silindi.");
+        }
+        [HttpPut]
+        public IActionResult UpdateWeatherCity(City city)
+        {
+            var value = context.Cities.Find(city.ID);
+            value.Name = city.Name;
+            value.Temperature = city.Temperature;
+            value.Country = city.Country;
+            value.Detail = city.Detail;
+            context.SaveChanges();
+            return Ok("Şehir güncellendi.");
+        }
+        [HttpGet("GetByIdWeatherCity")]
+        public IActionResult GetByIdWeatherCity (int id)
+        {
+            var value = context.Cities.Find(id);
+            return Ok(value);
+        }
+    }
+}
