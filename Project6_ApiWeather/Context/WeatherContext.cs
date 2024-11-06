3ffe2e9c60dd9836668e6550445780ff6351685c
Melih <mustafamelihcetin@outlook.com>
2024-11-06 03:18:44 +0300

    Migration process.

---
+++ Project6_ApiWeather/Context/WeatherContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project6_ApiWeather.Entities;
+
+namespace Project6_ApiWeather.Context
+{
+    public class WeatherContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DPCM; initial catalog=Db6Project20; integrated security=true;TrustServerCertificate=True;");            
+        }
+        public DbSet<City> Cities { get; set; }
+    }
+}
---
+++ Project6_ApiWeather/Entities/City.cs
@@ -0,0 +1,11 @@
+﻿namespace Project6_ApiWeather.Entities
+{
+    public class City
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public decimal Temperature { get; set; }
+        public string Detail { get; set; }
+    }
+}
