605ef70b5e72219663c8615df934b791e60b9124
Melih <mustafamelihcetin@outlook.com>
2024-11-10 01:37:10 +0300

    Menu design

--- Project7_ApiWeatherConsume/Program.cs
+++ Project7_ApiWeatherConsume/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Api Consume İşlemine Hoş Geldiniz");
+Console.WriteLine();
+x:
+Console.WriteLine("###   Yapmak istediğiniz işlemi seçiniz   ###");
+Console.WriteLine("### 1 - Şehir Listesini Getirme İşlemi");
+Console.WriteLine("### 2 - Yeni Şehir Ekleme İşlemi");
+Console.WriteLine("### 3 - Şehir Silme İşlemi");
+Console.WriteLine("### 4 - Şehir Güncelleme İşlemi");
+Console.WriteLine("### 5 - ID'ye Göre Şehir Getirme İşlemi");
+Console.WriteLine();
+
+Console.Write("Tercihiniz: ");
+int number;
+number = Convert.ToInt32(Console.ReadLine());
+
+switch(number)
+{
+    case 1:
+        Console.WriteLine("Şehir listesi: ");
+        break;
+    case 2:
+        Console.WriteLine("Yeni şehir ekleme alanı");
+        break;
+    case 3:
+        Console.WriteLine("Şehir silme alanı");
+        break;
+    case 4:
+        Console.WriteLine("Şehir güncelleme alanı");
+        break;
+    case 5:
+        break;
+    default:
+        Console.WriteLine("Geçerli bir seçenek giriniz.");
+        goto x;
+}
+
+
+
