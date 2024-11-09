50490f7c22766b4a94b73f70e30976e9befe53ea
Melih <mustafamelihcetin@outlook.com>
2024-11-10 02:16:40 +0300

    Adding new city process

--- Project7_ApiWeatherConsume/Program.cs
+++ Project7_ApiWeatherConsume/Program.cs
@@ -1,6 +1,8 @@
 ﻿#region Menu_Baslangici
 
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.Text;
 using System.Text.Json.Nodes;
 
 Console.WriteLine("Api Consume İşlemine Hoş Geldiniz");
@@ -8,9 +10,9 @@ Console.WriteLine();
 x:
 Console.WriteLine("###   Yapmak istediğiniz işlemi seçiniz   ###");
 Console.WriteLine("### 1 - Şehir Listesini Getirme İşlemi");
-Console.WriteLine("### 2 - Yeni Şehir Ekleme İşlemi");
-Console.WriteLine("### 3 - Şehir Silme İşlemi");
-Console.WriteLine("### 4 - Şehir Güncelleme İşlemi");
+Console.WriteLine("### 2 - Şehir ve sıcaklık Listesini Getirme İşlemi");
+Console.WriteLine("### 3 - Yeni Şehir Ekleme İşlemi");
+Console.WriteLine("### 4 - Şehir Silme İşlemi");
 Console.WriteLine("### 5 - ID'ye Göre Şehir Getirme İşlemi");
 Console.WriteLine();
 #endregion
@@ -44,17 +46,49 @@ switch (number)
             JArray jArray = JArray.Parse(responseBody);
             foreach (var item in jArray)
             {
-                string cityName = item["name"].ToString();
-                string temperature = item["temperature"].ToString();
-                string country = item["country"].ToString();
-                Console.WriteLine($"Şehir    : {cityName}");
-                Console.WriteLine($"Sıcaklık : {temperature}");
-                Console.WriteLine($"Ülke     : {country}\n");
+                string name2 = item["name"].ToString();
+                string temperature2 = item["temperature"].ToString();
+                string country2 = item["country"].ToString();
+                Console.WriteLine($"Şehir    : {name2}");
+                Console.WriteLine($"Sıcaklık : {temperature2}");
+                Console.WriteLine($"Ülke     : {country2}\n");
             }
         }
         break;
     case 3:
-        Console.WriteLine("Şehir silme alanı");
+        Console.WriteLine("### Yeni Veri Girişi ###\n");
+        string name3, country3, detail3;
+        decimal temperature;
+
+        Console.Write("Şehir Adı: ");
+        name3 = Console.ReadLine();
+
+        Console.Write("Ülke Adı: ");
+        country3 = Console.ReadLine();
+
+        Console.Write("Detay: ");
+        detail3 = Console.ReadLine();
+
+        Console.Write("Sıcaklık: ");
+        temperature = decimal.Parse(Console.ReadLine());
+
+        string url3 = "https://localhost:7000/api/Weathers";
+        var newWeatherCity = new
+        {
+            Name = name3,
+            Country = country3,
+            Detail = detail3,
+            Temperature = temperature
+
+        };
+        using (HttpClient client = new HttpClient())
+        {
+            string json = JsonConvert.SerializeObject(newWeatherCity);
+            StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await client.PostAsync(url3, content);
+            response.EnsureSuccessStatusCode();
+            
+        }
         break;
     case 4:
         Console.WriteLine("Şehir güncelleme alanı");
