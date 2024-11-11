962477c86776e675a38c4443ac1acac060414de2
Melih <mustafamelihcetin@outlook.com>
2024-11-12 01:58:53 +0300

    Applying SOLID principles.

--- Project7_ApiWeatherConsume/Program.cs
+++ Project7_ApiWeatherConsume/Program.cs
@@ -5,174 +5,199 @@ using Newtonsoft.Json.Linq;
 using System.Text;
 using System.Text.Json.Nodes;
 
-Console.WriteLine("Api Consume İşlemine Hoş Geldiniz");
-Console.WriteLine();
-x:
-Console.WriteLine("###   Yapmak istediğiniz işlemi seçiniz   ###");
-Console.WriteLine("### 1 - Şehir Listesini Getirme İşlemi");
-Console.WriteLine("### 2 - Şehir ve sıcaklık Listesini Getirme İşlemi");
-Console.WriteLine("### 3 - Yeni Şehir Ekleme İşlemi");
-Console.WriteLine("### 4 - Şehir Silme İşlemi");
-Console.WriteLine("### 5 - Veri Güncelleme İşlemi");
-Console.WriteLine("### 6 - ID'ye Göre Şehir Getirme İşlemi");
-Console.WriteLine();
-
-#endregion
-
-Console.Write("Tercihiniz: ");
-int number;
-number = Convert.ToInt32(Console.ReadLine());
-
-switch (number)
+public class Program
 {
-    case 1:
-        string url = "https://localhost:7000/api/Weathers";
+    private static readonly string baseUrl = "https://localhost:7000/api/Weathers";
+
+    public static async Task Main()
+    {
+        Console.WriteLine("Api Consume İşlemine Hoş Geldiniz\n");
+
+        while (true)
+        {
+            ShowMenu();
+            Console.Write("Tercihiniz: ");
+            int choice;
+            if (!int.TryParse(Console.ReadLine(), out choice))
+            {
+                Console.WriteLine("Geçerli bir seçenek giriniz.");
+                continue;
+            }
+
+            await HandleMenuChoice(choice);
+        }
+    }
+
+    private static void ShowMenu()
+    {
+        Console.WriteLine("###   Yapmak istediğiniz işlemi seçiniz   ###");
+        Console.WriteLine("### 1 - Şehir Listesini Getirme İşlemi");
+        Console.WriteLine("### 2 - Şehir ve Sıcaklık Listesini Getirme İşlemi");
+        Console.WriteLine("### 3 - Yeni Şehir Ekleme İşlemi");
+        Console.WriteLine("### 4 - Şehir Silme İşlemi");
+        Console.WriteLine("### 5 - Veri Güncelleme İşlemi");
+        Console.WriteLine("### 6 - ID'ye Göre Şehir Getirme İşlemi\n");
+    }
+
+    private static async Task HandleMenuChoice(int choice)
+    {
+        switch (choice)
+        {
+            case 1:
+                await GetCityList();
+                break;
+            case 2:
+                await GetCityAndTemperatureList();
+                break;
+            case 3:
+                await AddNewCity();
+                break;
+            case 4:
+                await DeleteCity();
+                break;
+            case 5:
+                await UpdateCity();
+                break;
+            case 6:
+                await GetCityById();
+                break;
+            default:
+                Console.WriteLine("Geçerli bir seçenek giriniz.");
+                break;
+        }
+    }
+
+    private static async Task GetCityList()
+    {
+        var cities = await FetchDataFromApi();
+        foreach (var city in cities)
+        {
+            Console.WriteLine($"Şehir: {city["name"]}");
+        }
+    }
+
+    private static async Task GetCityAndTemperatureList()
+    {
+        var cities = await FetchDataFromApi();
+        foreach (var city in cities)
+        {
+            Console.WriteLine($"Şehir: {city["name"]}, Sıcaklık: {city["temperature"]}, Ülke: {city["country"]}");
+        }
+    }
+
+    private static async Task AddNewCity()
+    {
+        var newCity = GetCityDetailsFromUser();
+        string json = JsonConvert.SerializeObject(newCity);
+
+        await PostDataToApi(json);
+        Console.WriteLine("Şehir başarıyla eklendi.");
+    }
+
+    private static async Task DeleteCity()
+    {
+        Console.Write("Silmek istediğiniz Şehir ID'sini giriniz: ");
+        int id = int.Parse(Console.ReadLine());
+        await DeleteDataFromApi(id);
+        Console.WriteLine("Şehir başarıyla silindi.");
+    }
+
+    private static async Task UpdateCity()
+    {
+        var updatedCity = GetCityDetailsFromUser();
+        updatedCity.ID = GetCityIdFromUser();
+        string json = JsonConvert.SerializeObject(updatedCity);
+
+        await PutDataToApi(json);
+        Console.WriteLine("Şehir bilgileri başarıyla güncellendi.");
+    }
+
+    private static async Task GetCityById()
+    {
+        int id = GetCityIdFromUser();
+        var city = await FetchDataFromApiById(id);
+
+        Console.WriteLine($"Şehir: {city["Name"]}, Ülke: {city["Country"]}, Sıcaklık: {city["Temperature"]}, Detay: {city["Detail"]}");
+    }
+
+    private static int GetCityIdFromUser()
+    {
+        Console.Write("Şehir ID'sini giriniz: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    private static async Task<JArray> FetchDataFromApi()
+    {
         using (HttpClient client = new HttpClient())
         {
-            HttpResponseMessage response = await client.GetAsync(url);
+            HttpResponseMessage response = await client.GetAsync(baseUrl);
+            response.EnsureSuccessStatusCode();
             string responseBody = await response.Content.ReadAsStringAsync();
-            JArray jArray = JArray.Parse(responseBody);
-            foreach (var item in jArray)
-            {
-                string cityName = item["name"].ToString();
-                Console.WriteLine($"Şehir: {cityName}");
-            }
+            return JArray.Parse(responseBody);
         }
-        break;
+    }
 
-    case 2:
-        string url2 = "https://localhost:7000/api/Weathers";
+    private static async Task<JObject> FetchDataFromApiById(int id)
+    {
         using (HttpClient client = new HttpClient())
         {
-            HttpResponseMessage response = await client.GetAsync(url2);
+            HttpResponseMessage response = await client.GetAsync($"{baseUrl}/GetByIdWeatherCity?id={id}");
+            response.EnsureSuccessStatusCode();
             string responseBody = await response.Content.ReadAsStringAsync();
-            JArray jArray = JArray.Parse(responseBody);
-            foreach (var item in jArray)
-            {
-                string name2 = item["name"].ToString();
-                string temperature2 = item["temperature"].ToString();
-                string country2 = item["country"].ToString();
-                Console.WriteLine($"Şehir    : {name2}");
-                Console.WriteLine($"Sıcaklık : {temperature2}");
-                Console.WriteLine($"Ülke     : {country2}\n");
-            }
+            return JObject.Parse(responseBody);
         }
-        break;
+    }
 
-    case 3:
-        Console.WriteLine("### Yeni Veri Girişi ###\n");
-        string name3, country3, detail3;
-        decimal temperature;
+    private static async Task PostDataToApi(string json)
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await client.PostAsync(baseUrl, content);
+            response.EnsureSuccessStatusCode();
+        }
+    }
 
+    private static async Task PutDataToApi(string json)
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await client.PutAsync(baseUrl, content);
+            response.EnsureSuccessStatusCode();
+        }
+    }
+
+    private static async Task DeleteDataFromApi(int id)
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            HttpResponseMessage response = await client.DeleteAsync($"{baseUrl}?id={id}");
+            response.EnsureSuccessStatusCode();
+        }
+    }
+
+    private static dynamic GetCityDetailsFromUser()
+    {
         Console.Write("Şehir Adı: ");
-        name3 = Console.ReadLine();
+        string name = Console.ReadLine();
 
         Console.Write("Ülke Adı: ");
-        country3 = Console.ReadLine();
+        string country = Console.ReadLine();
 
         Console.Write("Detay: ");
-        detail3 = Console.ReadLine();
+        string detail = Console.ReadLine();
 
         Console.Write("Sıcaklık: ");
-        temperature = decimal.Parse(Console.ReadLine());
+        decimal temperature = decimal.Parse(Console.ReadLine());
 
-        string url3 = "https://localhost:7000/api/Weathers";
-        var newWeatherCity = new
+        return new
         {
-            Name = name3,
-            Country = country3,
-            Detail = detail3,
+            Name = name,
+            Country = country,
+            Detail = detail,
             Temperature = temperature
         };
-        using (HttpClient client = new HttpClient())
-        {
-            string json = JsonConvert.SerializeObject(newWeatherCity);
-            StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
-            HttpResponseMessage response = await client.PostAsync(url3, content);
-            response.EnsureSuccessStatusCode();
-        }
-        break;
-
-    case 4:
-        string url4 = "https://localhost:7000/api/Weathers?id=";
-        Console.Write("Silmek istediğiniz ID: ");
-        int id = int.Parse(Console.ReadLine());
-        using (HttpClient client = new HttpClient())
-        {
-            HttpResponseMessage response = await client.DeleteAsync(url4 + id);
-            response.EnsureSuccessStatusCode();
-        }
-        break;
-
-    case 5:
-        Console.WriteLine("### Veri Güncelleme İşlemi ###\n");
-
-        string name4, country4, detail4;
-        decimal temperature3;
-        int id2;
-        Console.Write("Şehir Adı: ");
-        name4 = Console.ReadLine();
-
-        Console.Write("Ülke Adı: ");
-        country4 = Console.ReadLine();
-
-        Console.Write("Detay: ");
-        detail4 = Console.ReadLine();
-
-        Console.Write("Sıcaklık: ");
-        temperature3 = decimal.Parse(Console.ReadLine());
-
-        Console.Write("ID: ");
-        id2 = int.Parse(Console.ReadLine());
-
-        string url5 = "https://localhost:7000/api/Weathers";
-
-        var updatedValues = new
-        {
-            ID = id2,
-            Name = name4,
-            Country = country4,
-            Detail = detail4,
-            Temperature = temperature3
-        };
-
-        using (HttpClient client = new HttpClient())
-        {
-            string json = JsonConvert.SerializeObject(updatedValues);
-            StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
-            HttpResponseMessage response = await client.PutAsync(url5, content);
-            response.EnsureSuccessStatusCode();
-        }
-
-
-        break;
-
-    case 6:
-
-        string url6 = "https://localhost:7000/api/Weathers/GetByIdWeatherCity?id=";
-        Console.Write("İstediğiniz şehirini ID değeri: ");
-        int id3 = int.Parse(Console.ReadLine());
-        Console.WriteLine();
-        using(HttpClient client = new HttpClient())
-        {
-            HttpResponseMessage response = await client.GetAsync(url6 + id3);
-            response.EnsureSuccessStatusCode();
-            string responseBody = await response.Content.ReadAsStringAsync();
-            JObject weatherCityObject = JObject.Parse(responseBody);
-
-            string name = weatherCityObject["Name"].ToString();
-            string detail = weatherCityObject["Detail"].ToString();
-            string country = weatherCityObject["Country"].ToString();
-            decimal temp = decimal.Parse(weatherCityObject["Temperature"].ToString());
-            Console.WriteLine("Girmiş olduğunu ID değerine ait bilgiler: ");
-            Console.WriteLine();
-            Console.Write("Şehir: " + name + "\nÜlke: "+country+"\nSıcaklık: "+temp+"\nDetay: "+detail);
-        }
-        break;
-
-    default:
-        Console.WriteLine("Geçerli bir seçenek giriniz.");
-        goto x;
+    }
 }
-
-Console.Read();
+#endregion
