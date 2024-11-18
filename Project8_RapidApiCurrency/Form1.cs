a789d1f7bfda6399fe791ec18e7662ec82388f6b
Melih <mustafamelihcetin@outlook.com>
2024-11-19 00:35:22 +0300

    .

--- Project8_RapidApiCurrency/Form1.cs
+++ Project8_RapidApiCurrency/Form1.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -21,26 +22,87 @@ namespace Project8_RapidApiCurrency
 
         private async void Form1_Load(object sender, EventArgs e)
         {
-            var client = new HttpClient();
-            var request = new HttpRequestMessage
+            try
             {
-                Method = HttpMethod.Get,
-                RequestUri = new Uri("https://currency-conversion-and-exchange-rates.p.rapidapi.com/latest?from=TRY&to=USD%2CEUR"),
-                Headers =
-    {
-        { "x-rapidapi-key", "1edd51c834msh9ac8fb7335f0899p146fa2jsn3ac456265605" },
-        { "x-rapidapi-host", "currency-conversion-and-exchange-rates.p.rapidapi.com" },
-    },
-            };
-            using (var response = await client.SendAsync(request))
+                var client = new HttpClient();
+
+                // USD/TRY için API çağrısı
+                var requestUsd = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri("https://currency-conversion-and-exchange-rates.p.rapidapi.com/latest"),
+                    Headers =
             {
-                response.EnsureSuccessStatusCode();
-                var body = await response.Content.ReadAsStringAsync();
-                var json = JObject.Parse(body);
-                var value = json["rates: USD: Value"].ToString();
-                lblDollarSelling.Text = value;
-                //Console.WriteLine(body);
+                { "x-rapidapi-key", "1edd51c834msh9ac8fb7335f0899p146fa2jsn3ac456265605" },
+                { "x-rapidapi-host", "currency-conversion-and-exchange-rates.p.rapidapi.com" },
+            },
+                };
+
+                using (var responseUsd = await client.SendAsync(requestUsd))
+                {
+                    responseUsd.EnsureSuccessStatusCode();
+                    var bodyUsd = await responseUsd.Content.ReadAsStringAsync();
+                    var jsonUsd = JObject.Parse(bodyUsd);
+
+                    if (jsonUsd["rates"] != null && jsonUsd["rates"]["TRY"] != null)
+                    {
+                        var usdToTry = jsonUsd["rates"]["TRY"].ToString();
+                        lblDollarSelling.Text = usdToTry;
+                    }
+                    else
+                    {
+                        lblDollarSelling.Text = "USD/TRY verisi bulunamadı.";
+                    }
+                }
+
+                // EUR/TRY için API çağrısı
+                var requestEur = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri("https://currency-conversion-and-exchange-rates.p.rapidapi.com/latest"),
+                    Headers =
+            {
+                { "x-rapidapi-key", "1edd51c834msh9ac8fb7335f0899p194efaj25n3ac456205e5" },
+                { "x-rapidapi-host", "currency-conversion-and-exchange-rates.p.rapidapi.com" },
+            },
+                };
+
+                using (var responseEur = await client.SendAsync(requestEur))
+                {
+                    responseEur.EnsureSuccessStatusCode();
+                    var bodyEur = await responseEur.Content.ReadAsStringAsync();
+                    var jsonEur = JObject.Parse(bodyEur);
+
+                    if (jsonEur["rates"] != null && jsonEur["rates"]["TRY"] != null)
+                    {
+                        var eurToTry = jsonEur["rates"]["TRY"].ToString();
+                        lblEuroSelling.Text = eurToTry;
+                    }
+                    else
+                    {
+                        lblEuroSelling.Text = "EUR/TRY verisi bulunamadı.";
+                    }
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                // HTTP hatalarını yakala
+                MessageBox.Show($"HTTP hatası: {ex.Message}");
+            }
+            catch (JsonReaderException ex)
+            {
+                // JSON parse hatalarını yakala
+                MessageBox.Show($"JSON okuma hatası: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                // Diğer tüm hatalar için genel yakalama
+                MessageBox.Show($"Bir hata oluştu: {ex.Message}");
             }
         }
+
+
     }
+
 }
+
