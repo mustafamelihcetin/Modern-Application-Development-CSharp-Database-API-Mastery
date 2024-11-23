d6fc873c2759211cca5ae7339cc0ed135ba1541a
Melih <mustafamelihcetin@outlook.com>
2024-11-24 02:02:45 +0300

    Form design process.

--- Project10_PostgreSQLToDoListApp/Form1.Designer.cs
+++ Project10_PostgreSQLToDoListApp/Form1.Designer.cs
@@ -28,13 +28,257 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnList = new System.Windows.Forms.Button();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtHeader = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPriority = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.rdbCompleted = new System.Windows.Forms.RadioButton();
+            this.rdbContinue = new System.Windows.Forms.RadioButton();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnAllList = new System.Windows.Forms.Button();
+            this.btnCompleted = new System.Windows.Forms.Button();
+            this.btnContinue = new System.Windows.Forms.Button();
+            this.cmbCategory = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "İşlem ID: ";
+            // 
+            // btnList
+            // 
+            this.btnList.Location = new System.Drawing.Point(384, 16);
+            this.btnList.Name = "btnList";
+            this.btnList.Size = new System.Drawing.Size(144, 32);
+            this.btnList.TabIndex = 1;
+            this.btnList.Text = "Listele";
+            this.btnList.UseVisualStyleBackColor = true;
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(104, 16);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(264, 26);
+            this.txtID.TabIndex = 2;
+            // 
+            // txtHeader
+            // 
+            this.txtHeader.Location = new System.Drawing.Point(104, 56);
+            this.txtHeader.Name = "txtHeader";
+            this.txtHeader.Size = new System.Drawing.Size(264, 26);
+            this.txtHeader.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 56);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Başlık: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 96);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Kategori: ";
+            // 
+            // txtPriority
+            // 
+            this.txtPriority.Location = new System.Drawing.Point(104, 136);
+            this.txtPriority.Name = "txtPriority";
+            this.txtPriority.Size = new System.Drawing.Size(264, 26);
+            this.txtPriority.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 136);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Öncelik: ";
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(104, 176);
+            this.txtDescription.Multiline = true;
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(264, 128);
+            this.txtDescription.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 176);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Açıklama: ";
+            // 
+            // rdbCompleted
+            // 
+            this.rdbCompleted.AutoSize = true;
+            this.rdbCompleted.Location = new System.Drawing.Point(104, 320);
+            this.rdbCompleted.Name = "rdbCompleted";
+            this.rdbCompleted.Size = new System.Drawing.Size(113, 24);
+            this.rdbCompleted.TabIndex = 14;
+            this.rdbCompleted.TabStop = true;
+            this.rdbCompleted.Text = "Tamamlandı";
+            this.rdbCompleted.UseVisualStyleBackColor = true;
+            // 
+            // rdbContinue
+            // 
+            this.rdbContinue.AutoSize = true;
+            this.rdbContinue.Location = new System.Drawing.Point(104, 352);
+            this.rdbContinue.Name = "rdbContinue";
+            this.rdbContinue.Size = new System.Drawing.Size(125, 24);
+            this.rdbContinue.TabIndex = 15;
+            this.rdbContinue.TabStop = true;
+            this.rdbContinue.Text = "Devam Ediyor";
+            this.rdbContinue.UseVisualStyleBackColor = true;
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(384, 56);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(144, 32);
+            this.btnCreate.TabIndex = 16;
+            this.btnCreate.Text = "Oluştur";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(384, 96);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(144, 32);
+            this.btnDelete.TabIndex = 17;
+            this.btnDelete.Text = "Sil";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(384, 136);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(144, 32);
+            this.btnUpdate.TabIndex = 18;
+            this.btnUpdate.Text = "Güncelle";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnAllList
+            // 
+            this.btnAllList.Location = new System.Drawing.Point(384, 176);
+            this.btnAllList.Name = "btnAllList";
+            this.btnAllList.Size = new System.Drawing.Size(144, 32);
+            this.btnAllList.TabIndex = 19;
+            this.btnAllList.Text = "Tüm Liste";
+            this.btnAllList.UseVisualStyleBackColor = true;
+            // 
+            // btnCompleted
+            // 
+            this.btnCompleted.Location = new System.Drawing.Point(384, 216);
+            this.btnCompleted.Name = "btnCompleted";
+            this.btnCompleted.Size = new System.Drawing.Size(144, 32);
+            this.btnCompleted.TabIndex = 20;
+            this.btnCompleted.Text = "Tamamlananlar";
+            this.btnCompleted.UseVisualStyleBackColor = true;
+            // 
+            // btnContinue
+            // 
+            this.btnContinue.Location = new System.Drawing.Point(384, 256);
+            this.btnContinue.Name = "btnContinue";
+            this.btnContinue.Size = new System.Drawing.Size(144, 32);
+            this.btnContinue.TabIndex = 21;
+            this.btnContinue.Text = "Devam Edenler";
+            this.btnContinue.UseVisualStyleBackColor = true;
+            // 
+            // cmbCategory
+            // 
+            this.cmbCategory.FormattingEnabled = true;
+            this.cmbCategory.Location = new System.Drawing.Point(104, 96);
+            this.cmbCategory.Name = "cmbCategory";
+            this.cmbCategory.Size = new System.Drawing.Size(264, 28);
+            this.cmbCategory.TabIndex = 22;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(539, 408);
+            this.Controls.Add(this.cmbCategory);
+            this.Controls.Add(this.btnContinue);
+            this.Controls.Add(this.btnCompleted);
+            this.Controls.Add(this.btnAllList);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.rdbContinue);
+            this.Controls.Add(this.rdbCompleted);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtPriority);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtHeader);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.btnList);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Opacity = 0.95D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "To Do List";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnList;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtHeader;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPriority;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RadioButton rdbCompleted;
+        private System.Windows.Forms.RadioButton rdbContinue;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnAllList;
+        private System.Windows.Forms.Button btnCompleted;
+        private System.Windows.Forms.Button btnContinue;
+        private System.Windows.Forms.ComboBox cmbCategory;
     }
 }
 
