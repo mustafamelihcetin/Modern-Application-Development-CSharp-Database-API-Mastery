56afafeb6eb5c466e95c58579d2e19501c0d2640
Melih <mustafamelihcetin@outlook.com>
2024-11-29 00:56:36 +0300

    Updating category process is added.

--- Project10_PostgreSQLToDoListApp/FrmCategory.Designer.cs
+++ Project10_PostgreSQLToDoListApp/FrmCategory.Designer.cs
@@ -60,6 +60,7 @@
             this.btnUpdate.TabIndex = 27;
             this.btnUpdate.Text = "Güncelle";
             this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
             // 
             // btnDelete
             // 
--- Project10_PostgreSQLToDoListApp/FrmCategory.cs
+++ Project10_PostgreSQLToDoListApp/FrmCategory.cs
@@ -38,9 +38,9 @@ namespace Project10_PostgreSQLToDoListApp
 
         private void btnCreate_Click(object sender, EventArgs e)
         {
-            NpgsqlConnection connection= new NpgsqlConnection("Server=localHost;port=5432;Database=Db10Project20;user ID=postgres;Password=1234;");            
+            NpgsqlConnection connection = new NpgsqlConnection("Server=localHost;port=5432;Database=Db10Project20;user ID=postgres;Password=1234;");
             string query = "Insert Into categories (categoryname) values (@categoryName)";
-            using(var command = new NpgsqlCommand(query, connection))
+            using (var command = new NpgsqlCommand(query, connection))
             {
                 connection.Open();
                 command.Parameters.AddWithValue("@categoryName", txtHeader.Text);
@@ -55,12 +55,28 @@ namespace Project10_PostgreSQLToDoListApp
         {
             NpgsqlConnection connection = new NpgsqlConnection("Server=localHost;port=5432;Database=Db10Project20;user ID=postgres;Password=1234;");
             string query = "Delete From categories Where categoryid = @categoryId";
-            using(var command = new NpgsqlCommand(query,connection))
+            using (var command = new NpgsqlCommand(query, connection))
             {
                 connection.Open();
-                command.Parameters.AddWithValue("@categoryId",int.Parse(txtName.Text));
+                command.Parameters.AddWithValue("@categoryId", int.Parse(txtName.Text));
                 command.ExecuteNonQuery();
                 MessageBox.Show("Kategori Silindi.");
+                connection.Close();
+                CategoryList();
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            NpgsqlConnection connection = new NpgsqlConnection("Server=localHost;port=5432;Database=Db10Project20;user ID=postgres;Password=1234;");
+            string query = "Update categories SET categoryname = @categoryName Where categoryid = @categoryId";
+            using (var command = new NpgsqlCommand(query, connection))
+            {
+                connection.Open();
+                command.Parameters.AddWithValue("@categoryName", txtHeader.Text);
+                command.Parameters.AddWithValue("@categoryId", int.Parse(txtName.Text));
+                command.ExecuteNonQuery();
+                MessageBox.Show("Kategory Güncellendi.");
                 connection.Close();
                 CategoryList();
             }
