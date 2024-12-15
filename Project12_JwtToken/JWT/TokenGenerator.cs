a5a4b0fd07548cbb8c09895a76b9cba6b6b93a3c
Melih <mustafamelihcetin@outlook.com>
2024-12-16 00:53:49 +0300

    Creating token process.

--- Project12_JwtToken/JWT/TokenGenerator.cs
+++ Project12_JwtToken/JWT/TokenGenerator.cs
@@ -1,12 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Text;
-using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Project12_JwtToken.JWT
 {
     public class TokenGenerator
     {
+        public string GenerateJwtToken()
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("20Derste20ProjeToken++//1234tokenJwt"));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, "test01"),
+                new Claim(JwtRegisteredClaimNames.Email, "test@gmail.com"),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: "yourIssuer",
+                audience: "yourAudience",
+                claims: claims,
+                expires: DateTime.Now.AddHours(1),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
     }
 }
