bf65413322e6fe2c875815b26e779bd2074d6646
Melih <mustafamelihcetin@outlook.com>
2024-12-18 01:27:06 +0300

    Completing of the Project14.

--- Project14_SqlAllRelations/Form1.Designer.cs
+++ Project14_SqlAllRelations/Form1.Designer.cs
@@ -29,12 +29,91 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.teamsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.db14Project20DataSet = new Project14_SqlAllRelations.Db14Project20DataSet();
+            this.teamsTableAdapter = new Project14_SqlAllRelations.Db14Project20DataSetTableAdapters.TeamsTableAdapter();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.teamIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.teamNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.teamCountryDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.teamsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.db14Project20DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // teamsBindingSource
+            // 
+            this.teamsBindingSource.DataMember = "Teams";
+            this.teamsBindingSource.DataSource = this.db14Project20DataSet;
+            // 
+            // db14Project20DataSet
+            // 
+            this.db14Project20DataSet.DataSetName = "Db14Project20DataSet";
+            this.db14Project20DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // teamsTableAdapter
+            // 
+            this.teamsTableAdapter.ClearBeforeFill = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.teamIDDataGridViewTextBoxColumn,
+            this.teamNameDataGridViewTextBoxColumn,
+            this.teamCountryDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.teamsBindingSource;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(762, 491);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // teamIDDataGridViewTextBoxColumn
+            // 
+            this.teamIDDataGridViewTextBoxColumn.DataPropertyName = "TeamID";
+            this.teamIDDataGridViewTextBoxColumn.HeaderText = "TeamID";
+            this.teamIDDataGridViewTextBoxColumn.Name = "teamIDDataGridViewTextBoxColumn";
+            this.teamIDDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // teamNameDataGridViewTextBoxColumn
+            // 
+            this.teamNameDataGridViewTextBoxColumn.DataPropertyName = "TeamName";
+            this.teamNameDataGridViewTextBoxColumn.HeaderText = "TeamName";
+            this.teamNameDataGridViewTextBoxColumn.Name = "teamNameDataGridViewTextBoxColumn";
+            // 
+            // teamCountryDataGridViewTextBoxColumn
+            // 
+            this.teamCountryDataGridViewTextBoxColumn.DataPropertyName = "TeamCountry";
+            this.teamCountryDataGridViewTextBoxColumn.HeaderText = "TeamCountry";
+            this.teamCountryDataGridViewTextBoxColumn.Name = "teamCountryDataGridViewTextBoxColumn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(762, 491);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.teamsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.db14Project20DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private Db14Project20DataSet db14Project20DataSet;
+        private System.Windows.Forms.BindingSource teamsBindingSource;
+        private Db14Project20DataSetTableAdapters.TeamsTableAdapter teamsTableAdapter;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn teamIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn teamNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn teamCountryDataGridViewTextBoxColumn;
     }
 }
 
--- Project14_SqlAllRelations/Form1.cs
+++ Project14_SqlAllRelations/Form1.cs
@@ -16,5 +16,12 @@ namespace Project14_SqlAllRelations
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'db14Project20DataSet.Teams' table. You can move, or remove it, as needed.
+            this.teamsTableAdapter.Fill(this.db14Project20DataSet.Teams);
+
+        }
     }
 }
