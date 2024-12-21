08dcd864a8376b9edc435894c41e8eb83e94e35b
Melih <mustafamelihcetin@outlook.com>
2024-12-21 21:09:02 +0300

    Adding buttons dinamically.

--- Project19_TableStatusMomentary/Form1.cs
+++ Project19_TableStatusMomentary/Form1.cs
@@ -16,5 +16,23 @@ namespace Project19_TableStatusMomentary
         {
             InitializeComponent();
         }
+        Db19Project20Entities context = new Db19Project20Entities();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int buttonWidth = 150;
+            int buttonHeight = 75;
+            int padding = 10;
+            int xOffSet = 10;
+            int yOffSet = 10;
+
+            for (int i = 0; i < 12; i++)
+            {
+                Button button = new Button();
+                button.Text = $"Buton{i}";
+                button.Size = new Size(buttonWidth, buttonHeight);
+                button.Location = new Point(xOffSet + (i % 4) * (buttonWidth + padding), yOffSet + (i / 4) * (buttonHeight + padding));
+                this.Controls.Add(button);
+            }
+        }
     }
 }
