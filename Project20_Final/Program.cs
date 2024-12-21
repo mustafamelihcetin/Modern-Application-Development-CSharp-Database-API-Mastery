14d211e0c8983fabb5cf2a8ebc37b536654ba8fe
Melih <mustafamelihcetin@outlook.com>
2024-12-21 21:29:23 +0300

    The projects have been completed.

---
+++ Project20_Final/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project20_Final
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Eğitim sona erdi");
+            Console.ReadLine();
+        }
+    }
+}
